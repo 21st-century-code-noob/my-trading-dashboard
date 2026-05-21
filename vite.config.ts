65ba2a4aa624ff51e1ed53beda546e2bd56955a3
Xiaocheng Peng <woahipxc@gmail.com>
2026-05-21 14:45:28 +0800

    Fix absolute path import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
+import { fileURLToPath } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +11,6 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'), // 2. 定义 @ 映射到 src 目录
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))    },
   },
 })
