3da048da73858426b7c375231d97c1a6dca56385
Xiaocheng Peng <woahipxc@gmail.com>
2026-05-21 14:16:43 +0800

    Set up theming system Tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'), // 2. 定义 @ 映射到 src 目录
+    },
+  },
 })
